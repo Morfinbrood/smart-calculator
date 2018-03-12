ff9e7e125c41f5414e853999cf7f877466ea20f1
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-12 18:41:19 +0300

    implemented task, all tests passed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expr = String(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.expr = this.expr + "+" + number;
+    return this
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.expr = this.expr + "-" + number;
+    return this
   }
 
   multiply(number) {
-    // your implementation
+    this.expr = this.expr + "*" + number;
+    return this
   }
 
   devide(number) {
-    // your implementation
+    this.expr = this.expr + "/" + number;
+    return this
   }
 
   pow(number) {
-    // your implementation
+    this.expr = this.expr + "**" + number;
+    return this
   }
+  
+  valueOf() {
+    return eval(this.expr);
+  }
+
 }
 
 module.exports = SmartCalculator;
